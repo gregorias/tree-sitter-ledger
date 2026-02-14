gregorias/tree-sitter-ledger

commit 4ac9466a21d19f0076ea0393e3eb134c6b8fbddd

fix: improve handling of whitespace before codes
When multiple spaces were included before the xact code, the code was being
consumed and included as part of the payee.

This fixes the issue by clarifying that payees cannot start with a whitespace
char, and adds a test for this case.

grammar.js
@@ -255,7 +255,7 @@ module.exports = grammar({
 
         code: $ => seq('(', /[^)]*/, ')'),
 
-        payee: $ => /[^(*!\n;][^*!\n;]*/,
+        payee: $ => /[^(*!\S;][^*!\n;]*/,
 
         query: $ => /[^\n]+/,
 

commit 1fcad779cc53287796a69f99dfeab5c89ec1a3af

fix: properly handle whitespace in payee
4ac9466a21d19f0076ea0393e3eb134c6b8fbddd broke tests by mistakenly using
`\S`.

grammar.js
@@ -311,7 +311,7 @@ module.exports = grammar({
 
         code: $ => seq('(', /[^)]*/, ')'),
 
-        payee: $ => /[^(*!\S;][^\n;]*/,
+        payee: $ => /[^(*!\s;][^\n;]*/,
 
         query: $ => /[^\n]+/,
 
@@ -347,7 +347,7 @@ module.exports = grammar({
         amount: $ => {
             const quantity = seq(optional('+'), $.quantity);
             const negative_quantity = seq('-', $.negative_quantity);
-            
+
             return choice(
                 prec.right(1, seq(
                     choice(quantity, negative_quantity),
@@ -376,7 +376,7 @@ module.exports = grammar({
           seq( '{', optional($.whitespace), $.amount, optional($.whitespace), '}'),
           seq('{{', optional($.whitespace), $.amount, optional($.whitespace), '}}'),
         ),
-        
+
         price: $ => seq(
             choice('@', '@@'),
             optional($.whitespace),

src/tree_sitter/array.h
@@ -14,6 +14,7 @@ extern "C" {
 #include <string.h>
 
 #ifdef _MSC_VER
+#pragma warning(push)
 #pragma warning(disable : 4101)
 #elif defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic push
@@ -278,7 +279,7 @@ static inline void _array__splice(Array *self, size_t element_size,
 #define _compare_int(a, b) ((int)*(a) - (int)(b))
 
 #ifdef _MSC_VER
-#pragma warning(default : 4101)
+#pragma warning(pop)
 #elif defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic pop
 #endif